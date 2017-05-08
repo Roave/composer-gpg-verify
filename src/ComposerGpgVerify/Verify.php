0cdf863e4964796c30f9bf64b14eebb81637665c
Marco Pivetta <ocramius@gmail.com>
2017-05-09 01:15:59 +0200

    Stubbing out a "verify" class - just prototyping for now

---
+++ src/ComposerGpgVerify/Verify.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ComposerGpgVerify;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+final class Verify implements PluginInterface, EventSubscriberInterface
+{
+    private function __construct()
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents() : array
+    {
+        return [
+            ScriptEvents::PRE_AUTOLOAD_DUMP => 'verify',
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        // Nothing to do here, as all features are provided through event listeners
+    }
+
+    public static function verify(Event $composerEvent) : void
+    {
+        $composer  = $composerEvent->getComposer();
+        $vendorDir = $composer->getConfig()->get('vendor-dir');
+
+        // @todo that `$vendorDir` may include special chars
+        /* @var $vendorDirs \GlobIterator|\SplFileInfo[]*/
+        $vendorDirs = new \GlobIterator(
+            $vendorDir . '/*/*',
+            \FilesystemIterator::KEY_AS_PATHNAME | \FilesystemIterator::FOLLOW_SYMLINKS | \FilesystemIterator::SKIP_DOTS | \FilesystemIterator::CURRENT_AS_FILEINFO
+        );
+
+        foreach ($vendorDirs as $vendorDir) {
+            if (! $vendorDir->isDir()) {
+                var_dump('Not a dir: ' . $vendorDir->getPath());
+
+                continue;
+            }
+
+            var_dump($vendorDir->getPath()); // @TODO will need to check that it is a dir
+        }
+    }
+}

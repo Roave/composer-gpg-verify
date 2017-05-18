9e59ce2a045b5fcfacdcc1c7a9efcc1c9cd9afae
Marco Pivetta <ocramius@gmail.com>
2017-05-18 21:17:18 +0200

    Hardening example script

--- examples/successful-installation.sh
+++ examples/successful-installation.sh
@@ -7,18 +7,18 @@ LIBRARY_COMMIT="$( git rev-parse HEAD )"
 
 # we work in a temporary directory
 BASEDIR="$( mktemp -d )"
-
-mkdir "$BASEDIR/gnupg-home"
+COMPOSER_PHAR="$BASEDIR/composer.phar"
 
 # let's not mess with your system's GPG environment :-)
 export GNUPGHOME="$BASEDIR/gnupg-home"
+mkdir "$GNUPGHOME"
+chmod 0700 "$GNUPGHOME"
 
 curl -sS https://getcomposer.org/installer | php --
-mv composer.phar "$BASEDIR/composer.phar"
+mv composer.phar "$COMPOSER_PHAR"
 
 # this is ocramius@gmail.com's GPG key
 gpg --keyserver pgp.mit.edu --recv-keys 7BB9B50FE8E021187914A5C09F65FBC212EC2DF8
-echo "trust\ny" > trust-key-input.txt
 
 # we need to tell composer where to find this repository, as we want to test against the current HEAD
 cat > "$BASEDIR/composer.json" << JSON
@@ -36,13 +36,11 @@ JSON
 # this is just a non-interactive way to run `gpg --edit-key` and then typing "trust", "4", "y"
 echo -e "trust\n4\ny\n" | gpg --command-fd 0 --edit-key 7BB9B50FE8E021187914A5C09F65FBC212EC2DF8
 
-cd $BASEDIR
-
 # let's install this library first
-$BASEDIR/composer.phar --working-dir="$BASEDIR" require "roave/composer-gpg-verify:dev-master#$LIBRARY_COMMIT" --prefer-source
+"$COMPOSER_PHAR" --working-dir="$BASEDIR" require "roave/composer-gpg-verify:dev-master#$LIBRARY_COMMIT" --prefer-source
 
 # this package installation will work, because this guy knows how to do releases, and this release is signed
-$BASEDIR/composer.phar --working-dir="$BASEDIR" require ocramius/package-versions:1.1.2 --prefer-source
+"$COMPOSER_PHAR" --working-dir="$BASEDIR" require ocramius/package-versions:1.1.2 --prefer-source
 
 # this commit is not signed - this command should fail
-$BASEDIR/composer.phar --working-dir="$BASEDIR" require "ocramius/lazy-map:dev-master#5c77102e225d225ae2d74d5f2cc488527834b821"
+"$COMPOSER_PHAR" --working-dir="$BASEDIR" require "ocramius/lazy-map:dev-master#5c77102e225d225ae2d74d5f2cc488527834b821" --prefer-source

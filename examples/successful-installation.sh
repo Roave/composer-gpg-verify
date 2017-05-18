774c3e5a504792066e92abf5b26c00fc7e2400c4
Marco Pivetta <ocramius@gmail.com>
2017-05-18 21:08:54 +0200

    Adding \(possible\) example script to run installation and check that everything is OK

---
+++ examples/successful-installation.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+IFS=$'\n\t'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIBRARY_COMMIT="$( git rev-parse HEAD )"
+
+# we work in a temporary directory
+BASEDIR="$( mktemp -d )"
+
+mkdir "$BASEDIR/gnupg-home"
+
+# let's not mess with your system's GPG environment :-)
+export GNUPGHOME="$BASEDIR/gnupg-home"
+
+curl -sS https://getcomposer.org/installer | php --
+mv composer.phar "$BASEDIR/composer.phar"
+
+# this is ocramius@gmail.com's GPG key
+gpg --keyserver pgp.mit.edu --recv-keys 7BB9B50FE8E021187914A5C09F65FBC212EC2DF8
+echo "trust\ny" > trust-key-input.txt
+
+# we need to tell composer where to find this repository, as we want to test against the current HEAD
+cat > "$BASEDIR/composer.json" << JSON
+{
+    "minimum-stability": "dev",
+    "repositories": [
+        {
+            "type": "vcs",
+            "url": "$DIR/../"
+        }
+    ]
+}
+JSON
+
+# this is just a non-interactive way to run `gpg --edit-key` and then typing "trust", "4", "y"
+echo -e "trust\n4\ny\n" | gpg --command-fd 0 --edit-key 7BB9B50FE8E021187914A5C09F65FBC212EC2DF8
+
+cd $BASEDIR
+
+# let's install this library first
+$BASEDIR/composer.phar --working-dir="$BASEDIR" require "roave/composer-gpg-verify:dev-master#$LIBRARY_COMMIT" --prefer-source
+
+# this package installation will work, because this guy knows how to do releases, and this release is signed
+$BASEDIR/composer.phar --working-dir="$BASEDIR" require ocramius/package-versions:1.1.2 --prefer-source
+
+# this commit is not signed - this command should fail
+$BASEDIR/composer.phar --working-dir="$BASEDIR" require "ocramius/lazy-map:dev-master#5c77102e225d225ae2d74d5f2cc488527834b821"

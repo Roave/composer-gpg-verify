5c7c796a9698aaa501a820d3b6bf260c5b812e6e
Marco Pivetta <ocramius@gmail.com>
2017-05-18 21:40:14 +0200

    Fully working installation example, besides last step (to be checked)

--- examples/successful-installation.sh
+++ examples/successful-installation.sh
@@ -8,6 +8,7 @@ LIBRARY_COMMIT="$( git rev-parse HEAD )"
 # we work in a temporary directory
 BASEDIR="$( mktemp -d )"
 COMPOSER_PHAR="$BASEDIR/composer.phar"
+export COMPOSER_HOME="$BASEDIR"
 
 # let's not mess with your system's GPG environment :-)
 export GNUPGHOME="$BASEDIR/gnupg-home"
@@ -20,10 +21,16 @@ mv composer.phar "$COMPOSER_PHAR"
 # this is ocramius@gmail.com's GPG key
 gpg --keyserver pgp.mit.edu --recv-keys 7BB9B50FE8E021187914A5C09F65FBC212EC2DF8
 
+# this is just a non-interactive way to run `gpg --edit-key` and then typing "trust", "4", "y"
+echo -e "trust\n5\ny\nsave" | gpg --command-fd 0 --edit-key 7BB9B50FE8E021187914A5C09F65FBC212EC2DF8
+
 # we need to tell composer where to find this repository, as we want to test against the current HEAD
 cat > "$BASEDIR/composer.json" << JSON
 {
     "minimum-stability": "dev",
+    "config": {
+        "preferred-install": "source"
+    },
     "repositories": [
         {
             "type": "vcs",
@@ -32,9 +39,6 @@ cat > "$BASEDIR/composer.json" << JSON
     ]
 }
 JSON
-
-# this is just a non-interactive way to run `gpg --edit-key` and then typing "trust", "4", "y"
-echo -e "trust\n4\ny\n" | gpg --command-fd 0 --edit-key 7BB9B50FE8E021187914A5C09F65FBC212EC2DF8
 
 # let's install this library first
 "$COMPOSER_PHAR" --working-dir="$BASEDIR" require "roave/composer-gpg-verify:dev-master#$LIBRARY_COMMIT" --prefer-source

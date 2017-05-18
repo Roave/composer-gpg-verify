e4cc0a89fed231cd27d199e10a9678e3d7505fb2
Marco Pivetta <ocramius@gmail.com>
2017-05-18 10:15:40 +0200

    Checking parsing of good, but un-trusted signature

--- test/RoaveTest/ComposerGpgVerify/Package/Git/GitSignatureCheckTest.php
+++ test/RoaveTest/ComposerGpgVerify/Package/Git/GitSignatureCheckTest.php
@@ -230,7 +230,7 @@ gpg: Signature made Do 18 Mai 2017 10:08:45 CEST
 gpg:                using RSA key 4B95C0CE4DE340CC
 gpg: Can't check signature: No public key
 OUTPUT
-,
+                ,
                 false,
                 <<<'READABLE'
 [SIGNED] [NOT VERIFIED] Commit #bf2fabeabe00f14f0ce0090adc7a2b9b770edbe3 Tag tag-name  (Key 4B95C0CE4DE340CC)
@@ -246,6 +246,50 @@ gpg: keybox '/tmp/gpg-verification-test591d568c5d0947.51554486//pubring.kbx' cre
 gpg: Signature made Do 18 Mai 2017 10:08:45 CEST
 gpg:                using RSA key 4B95C0CE4DE340CC
 gpg: Can't check signature: No public key
+READABLE
+            ],
+            'failed verification - signed tag, untrusted key' => [
+                $package,
+                'git tag -v tag-name',
+                1,
+                <<<'OUTPUT'
+object be3b7a6f0ee4d90a72e1e1a19f89d8eeef746200
+type commit
+tag tag-name
+tagger Mr. Magoo <magoo@example.com> 1495095245 +0200
+
+signed tag
+gpg: Signature made Do 18 Mai 2017 10:14:05 CEST
+gpg:                using RSA key 865E20A60B500B00
+gpg: checking the trustdb
+gpg: marginals needed: 3  completes needed: 1  trust model: pgp
+gpg: depth: 0  valid:   1  signed:   0  trust: 0-, 0q, 0n, 0m, 0f, 1u
+gpg: Good signature from "Mr. Magoo <magoo@example.com>" [unknown]
+gpg: WARNING: This key is not certified with a trusted signature!
+gpg:          There is no indication that the signature belongs to the owner.
+Primary key fingerprint: D8BE 3E96 4271 2378 9551  AF87 865E 20A6 0B50 0B00
+OUTPUT
+                ,
+                false,
+                <<<'READABLE'
+[SIGNED] [NOT VERIFIED] Commit #be3b7a6f0ee4d90a72e1e1a19f89d8eeef746200 Tag tag-name By "Mr. Magoo <magoo@example.com>" (Key 865E20A60B500B00)
+Command: git tag -v tag-name
+Exit code: 1
+Output: object be3b7a6f0ee4d90a72e1e1a19f89d8eeef746200
+type commit
+tag tag-name
+tagger Mr. Magoo <magoo@example.com> 1495095245 +0200
+
+signed tag
+gpg: Signature made Do 18 Mai 2017 10:14:05 CEST
+gpg:                using RSA key 865E20A60B500B00
+gpg: checking the trustdb
+gpg: marginals needed: 3  completes needed: 1  trust model: pgp
+gpg: depth: 0  valid:   1  signed:   0  trust: 0-, 0q, 0n, 0m, 0f, 1u
+gpg: Good signature from "Mr. Magoo <magoo@example.com>" [unknown]
+gpg: WARNING: This key is not certified with a trusted signature!
+gpg:          There is no indication that the signature belongs to the owner.
+Primary key fingerprint: D8BE 3E96 4271 2378 9551  AF87 865E 20A6 0B50 0B00
 READABLE
             ],
         ];

9b21fba44ece5292d251183ee8c1ec165bc6ddee
Marco Pivetta <ocramius@gmail.com>
2017-05-18 11:55:18 +0200

    Only tags on the current commit are considered for validity checking

--- test/RoaveTest/ComposerGpgVerify/VerifyTest.php
+++ test/RoaveTest/ComposerGpgVerify/VerifyTest.php
@@ -382,6 +382,28 @@ final class VerifyTest extends TestCase
         $this->assertWillFailPackageVerification();
     }
 
+    public function testWillRejectSignedTagsFromNonHeadCommit() : void
+    {
+        $gpgHome     = $this->makeGpgHomeDirectory();
+        $vendorName  = 'Mr. Magoo';
+        $vendorEmail = 'magoo@example.com';
+        $vendorKey   = $this->makeKey($gpgHome, $vendorEmail, $vendorName);
+        $vendorDir   = $this->makeVendorDirectory();
+        $vendor1     = $this->makeDependencyGitRepository($vendorDir, 'vendor1/package1', $vendorEmail, $vendorName);
+
+        $this->createDependencySignedTag($vendor1, $gpgHome, $vendorKey);
+
+        (new Process('git commit --allow-empty -m "unsigned commit"', $vendor1))
+            ->setTimeout(30)
+            ->mustRun();
+
+        $this->configureCorrectComposerSetup();
+
+        putenv('GNUPGHOME=' . $gpgHome);
+
+        $this->assertWillFailPackageVerification();
+    }
+
     public function testWillOnlyConsiderTheHeadCommitForValidation() : void
     {
         $gpgHome     = $this->makeGpgHomeDirectory();
